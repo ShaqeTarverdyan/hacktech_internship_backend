882ec822c39cee19811967b1211b30f6aa135919
ShaqeTarverdyan <tshaqe@gmail.com>
2020-11-06 17:50:58 +0400

    Use validator methods in route

--- routes/admin.js
+++ routes/admin.js
@@ -1,56 +1,22 @@
 const express = require('express');
-const { body } = require('express-validator');
 const router = express.Router();
-const Admin = require('../models/admin');
 const adminController = require('../controllers/admin');
+const userFullValidator = require('../validations/signUpValidator');
+const loginValidator = require('../validations/loginValidator');
 
 router.post(
     '/admin', 
-    [
-        body('firstname')
-            .trim()
-            .notEmpty(),
-        body('lastname')
-            .trim()
-            .notEmpty(),
-        body('email')
-            .trim()
-            .isEmail()
-            .withMessage('Please enter a valid email')
-            .normalizeEmail(),
-        body('password')
-            .trim()
-            .isLength({ min: 3 })
-            .withMessage('must be at least 6 chars long')
-            .notEmpty(),
-        body('role')
-            .trim()
-            .notEmpty()
-    ],
+    userFullValidator(),
     adminController.registerNewAdmin);
 router.get('/admins', adminController.getAdmins);
 router.get('/admin/:adminId', adminController.getAdmin);
 router.put(
     '/admin/:adminid', 
-    [
-        body('firstname')
-            .trim()
-            .notEmpty(),
-        body('lastname')
-            .trim()
-            .notEmpty(),
-        body('email')
-            .trim()
-            .isEmail()
-            .notEmpty(),
-        body('role')
-            .trim()
-            .notEmpty()
-    ],
+    userFullValidator(),
     adminController.updateAdmin
 );
 router.delete('/admin/:adminId', adminController.deleteAdmin)
-router.post('/login', adminController.loginAdmin);
+router.post('/login',loginValidator(), adminController.loginAdmin);
 router.put(
     "/activateAdmin/:id",
     adminController.togglePanelAdminStatus
